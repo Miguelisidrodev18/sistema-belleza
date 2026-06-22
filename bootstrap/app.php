872369b41b8117027c6624199fcb6a177857fb1a
Miguel Isidro <74093841@continental.edu.pe>
2026-06-22 13:06:42 -0500

    Errror 419 correfgido

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,5 +19,8 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (\Illuminate\Session\TokenMismatchException $e, $request) {
+            return redirect()->route('login')
+                ->withErrors(['email' => 'Tu sesión ha expirado. Por favor inicia sesión de nuevo.']);
+        });
     })->create();
